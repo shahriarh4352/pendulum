5e9117c8c3ecbe304f3aeedc5a3df0d65a20e69d
shahriarh4352 <shahriarh4352@gmail.com>
2024-09-09 18:12:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,6 @@ console.log("JavaScript is working!");
 // Select all sections
 const sections = document.querySelectorAll('.section');
 
-// Create a new Intersection Observer instance
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        // When the section enters the viewport, add 'active' class
-        if (entry.isIntersecting) {
-            entry.target.classList.add('active');
-        }
-    });
-});
-
-// Observe each section
-sections.forEach((section) => {
-    observer.observe(section);
-});
+// Prevent cache by adding a timestamp to resources
+const cssLink = document.querySelector('link[rel="stylesheet"]');
+cssLink.href = cssLink.href + '?v=' + new Date().getTime();
