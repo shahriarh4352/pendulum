406adc676ab915e7fc45ba96701af030b8bb0b03
shahriarh4352 <shahriarh4352@gmail.com>
2024-09-10 06:51:41 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log("JavaScript is working!");
-// Select all sections
-const sections = document.querySelectorAll('.section');
-
-// Prevent cache by adding a timestamp to resources
-const cssLink = document.querySelector('link[rel="stylesheet"]');
-cssLink.href = cssLink.href + '?v=' + new Date().getTime();
